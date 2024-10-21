3235724c77c76c5e028a414f5578b44073be632b
Vicente Duch <vhduch@gmail.com>
2024-10-21 16:22:33 +0200

    done - vicente duch

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
 
-  get(pos) {}
+    let i;
 
-  max() {}
+    for (i = 0; i < this.items.length; i++) {
+      if (this.items[i] >= item) {
+        break;
+      }
+    }
+    this.items.splice(i, 0, item);
+    this.length = this.items.length;
+  }
 
-  min() {}
+  get(pos) {
+    if (pos < 0 || pos >= this.items.length) {
+      throw new Error("OutOfBounds")
+    } else {
+      return this.items[pos]
+    }
+  }
 
-  sum() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.items[this.items.length - 1]
+    }
+  }
 
-  avg() {}
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.items[0]
+    }
+  }
+
+  sum() {
+    let total = 0
+
+    for (let i = 0; i < this.items.length; i++) {
+      total += this.items[i]
+    }
+    return total
+
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return (this.sum() / this.items.length)
+  }
 }
 
 module.exports = SortedList;
